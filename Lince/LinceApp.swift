6d37325925849b11938ee4c95e5f15460f298c7a
Leon Sanchez <leonpsanchez@gmail.com>
2021-05-14 18:43:13 -0300

    Initial Commit

---
+++ Lince/LinceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinceApp.swift
+//  Lince
+//
+//  Created by Leon Sanchez on 14/05/21.
+//
+
+import SwiftUI
+
+@main
+struct LinceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
